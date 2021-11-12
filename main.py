d39b9417e7fe06dabdf3c9a99e61f3ddb74e32d7
Ernest Morse <enmorse48@gmail.com>
2021-11-12 12:43:30 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create another canvas, and this time, draw a triangle.
+# Lok back at the diagram of the circle with degrees
+# ("Moving the Turtle" on page 46) to remind yourself
+# which direction to turn the turtle using degrees.
+import turtle
+
+t = turtle.Pen()
+
+t.forward(190)
+t.left(135)
+t.forward(135)
+t.left(90)
+t.forward(135)
+t.left(135)
